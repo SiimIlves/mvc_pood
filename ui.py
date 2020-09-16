c766f4c42d82f7756d1afbdd1687d5aafa3b3e40
Siim Ilves <siim.ilves.001@khk.ee>
2020-09-16 11:17:18 +0300

    Created and tested product description

--- ui.py
+++ ui.py
@@ -2,13 +2,17 @@
 
 #import classes and files
 from product import Product
+from shop import Shop
 
 #create products
 bread = Product("bread", 0.80, 10)
-milk = product("milk", 0.50, 50)
+milk = Product("milk", 0.50, 50)
 wine = Product("wine", 5.60, 5)
 
-# test product view
-print(bread)
-print(milk)
-print(wine)
+# create shop and add products to it
+shop = Shop()
+shop.addProduct(bread)
+shop.addProduct(milk)
+shop.addProduct(wine)
+# test shop content
+print(shop)
