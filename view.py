00f8d083aeadae68994059c3a89132725431c6ce
SiimIlves <65825538+SiimIlves@users.noreply.github.com>
2020-09-24 23:53:44 +0300

    uuendus

--- view.py
+++ view.py
@@ -11,3 +11,19 @@ class View:
                   str(item.getAmount()))
             print("---------------------------")
         print("============================")
+        
+    # show item
+    def showItem(self, item):
+        print("Item -{}-".format(item.getName()))
+        print("============================")
+        print("name\t|\tprice\t|\tamount")
+        print(item.getName()+"\t|\t"+
+                  str(item.getPrice())+"\t\t|\t"+
+                  str(item.getAmount()))
+        print("============================")
+
+    # no item error
+    def noItemError(self, name):
+        print("============================")
+        print("Shop is not selling {}.".format(name))
+        print("============================")
